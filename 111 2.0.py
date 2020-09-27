e83f062aa11959668c0e24ba6ca8814767a4338b
italbey <71821200+italbey@users.noreply.github.com>
2020-09-27 17:47:15 +0300

    Add files via upload

---
+++ 111 2.0.py
@@ -0,0 +1,52 @@
+name = input("Назови своё имя ")
+print("Хєлоу,", name,)
+num = input ("Не хочеш посмотреть мемы с енотами?" )
+if int != "i ne lox":
+    print ("Вопрос был риторическим вот, держи \nhttps://www.youtube.com/watch?v=M3pDGot68xU&ab_channel=%D0%97%D0%B2%D0%B5%D1%80%D1%8E%D0%B3%D0%B8%D0%A2%D0%92 \nладно,шутки щутками,а сейчас пришло время показать тебе мои супер-пупер навыки анализа чисел")
+num = float (input ("введите число: "))
+if num > 0:
+    print ("Вы ввели число больше 0")
+    if num >= 10:
+         print ("вы ввели число больше или = 10")
+    if num >= 25:
+        print ("вы ввели число больше или = 25")
+    if num >= 60:
+        print ("вы ввели число больше или = 60")
+    if num >= 100:
+        print ("вы ввели число больше или = 100")
+    if num >= 150:
+        print ("вы ввели число больше или = 150")
+if num <= 0:
+        print ("вы ввели число меньше или = 0")
+if num <= -10:
+        print ("вы ввели число меньше или = -10")
+if num <= -25:
+         print ("вы ввели число меньше или = -25")
+if num <= -60:
+        print ("вы ввели число меньше или = -60")
+if num <= -100:
+        print ("вы ввели число меньше или = -100")
+if num <= -150:
+        print ("вы ввели число меньше или = -150")
+
+print (" Самоуничтожение программы через: ")
+print (" 5")
+import time
+time.sleep(1)
+print ("4")
+import time
+time.sleep(1)
+print (" 3")
+import time
+time.sleep(1)
+print ("2")
+import time
+time.sleep(1)
+print (" 1")
+import time
+time.sleep(1.5)
+
+exit (0)
+
+
+input ()
